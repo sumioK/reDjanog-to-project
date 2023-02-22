994205181a2b168c4398a4334388091da3431144
sumiok <tap.miwo@gmail.com>
2023-02-22 12:25:09 +0900

    detail

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Todomodel
+
 class TodoList(ListView):
     template_name = 'list.html'
+    model = Todomodel
+
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
     model = Todomodel
