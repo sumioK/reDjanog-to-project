d1f97b6c278719f6fb709aa1d39e7a6aec970047
sumiok <tap.miwo@gmail.com>
2023-02-22 17:08:44 +0900

    create and delete

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Todomodel
 
 class TodoList(ListView):
@@ -9,3 +10,14 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = Todomodel
+
+class TodoCreate(CreateView):
+  template_name = 'create.html'
+  model = Todomodel
+  fields = ('title', 'memo', 'duedate', 'priority')
+  success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+   template_name = 'delete.html'
+   model = Todomodel
+   success_url = reverse_lazy('list')
