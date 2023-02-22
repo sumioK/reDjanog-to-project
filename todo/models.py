387165e301a503f800cf3f31c1adc3ca306a8755
sumiok <tap.miwo@gmail.com>
2023-02-22 13:21:18 +0900

    add column

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+CHOICE = ('danger','high'), ('warning', 'normal'), ('primary', 'row')
+
 class Todomodel(models.Model):
     title = models.CharField(max_length=50)
     memo = models.TextField()
-
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE
+        )
+    duedate = models.DateField()
+    
     def __str__(self):
         return str(self.id) + '.' + self.title
