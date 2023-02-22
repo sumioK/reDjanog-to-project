c463a3258e3786b5bb2f1971fd632a7029cb3e76
sumiok <tap.miwo@gmail.com>
2023-02-22 12:14:57 +0900

    list

--- todo/models.py
+++ todo/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Todomodel(models.Model):
     title = models.CharField(max_length=50)
     memo = models.TextField()
+
+    def __str__(self):
+        return str(self.id) + '.' + self.title
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic.list import ListView
+from .models import Todomodel
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = Todomodel
